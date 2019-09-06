53c7496655b37901d3a34512ee593bf079fdd8c5
acmark527 <asdas>
2019-09-05 21:36:28 -0400

    Created Wrapper Tile class

--- Metroid/Metroid/Tile.cpp
+++ Metroid/Metroid/Tile.cpp
@@ -2,8 +2,56 @@
 
 
 
-Tile::Tile()
-{
+Tile::Tile(SDL_Rect initPosition, SDL_Rect initSource, int hitboxOffsetX, int hitboxOFfsetY, TileType Type, TileOrientation Orientation, TileSetting Setting) :
+	GameObject(initPosition, initSource, hitboxOffsetX, hitboxOFfsetY){
+	int sourceX = 0;
+	int sourceY = 0;
+	type = Type;
+	orientation = Orientation;
+	setting = Setting;
+
+	switch (setting){
+	case Caverns: //Cavern initalization
+		switch(type){
+		case Platform:
+			switch (orientation){
+			case TopLeft:
+				break;
+			case TopRight:
+				break;
+			case TopMiddle:
+				break;
+			case Left:
+				break;
+			case Right:
+				break;
+			case Middle:
+				break;
+			case CenterHorizontal:
+				break;
+			case CenterVertical:
+				break;
+			case Bottom:
+				break;
+			}
+			break;
+		case Bounce:
+			break;
+		case Spike:
+			break;
+		case Swap:
+			break;
+		case Passable:
+			break;
+		case Swing:
+			break;
+		case Move:
+			break;
+		}
+		break;
+
+
+	}
 }
 
 
--- Metroid/Metroid/Tile.h
+++ Metroid/Metroid/Tile.h
@@ -2,19 +2,41 @@
 #include "GameObject.h"
 
 enum TileType {
-
-
+	Bounce,
+	Swap,
+	Passable,
+	Spike,
+	Swing,
+	Move,
+	Platform
 };
 
 enum TileOrientation {
+	TopMiddle,
+	TopLeft,
+	TopRight,
+	Left,
+	Right,
+	Middle,
+	Bottom,
+	CenterHorizontal,
+	CenterVertical
+};
 
+enum TileSetting {
+	Caverns
 
 };
 
 class Tile : GameObject
 {
 public:
-	Tile();
+	Tile(SDL_Rect initPosition, SDL_Rect initSource, int hitboxOffsetX, int hitboxOFfsetY, TileType type, TileOrientation orientation, TileSetting setting);
 	~Tile();
+
+private:
+	TileType type;
+	TileOrientation orientation;
+	TileSetting setting;
 };
 
